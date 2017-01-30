dd1bcc031970d66e063bdec629b752671ffc507b
Subash Maharjan <hudeomah@gmail.com>
2017-01-30 12:12:53 -0500

    Slideshow.js cleaned

--- marsdrupal/themes/custom/mars/js/slideshow.js
+++ marsdrupal/themes/custom/mars/js/slideshow.js
@@ -5,32 +5,13 @@
     Drupal.behaviors.slideShow = {
         attach: function(context, settings){
             $(document).ready(function(){
-                $(document).ready(function(){
-                    $('.flexslider').flexslider({
-                        animation: "slide",
-                        directionNav: true,
-                        slideshow: false,
-                    });
+                $('.flexslider').flexslider({
+                    animation: "slide",
+                    directionNav: true,
+                    slideshow: false,
                 });
             });
         }
     }
 
 })(jQuery, Drupal);
-
-// Drupal.behaviors.myBehavior = {
-//     attach: function (context, settings) {
-//         // Using once() to apply the myCustomBehaviour effect when you want to do just run one function.
-//         $(context).find('input.myCustomBehavior').once('myCustomBehavior').addClass('processed');
-//
-//         // Using once() with more complexity.
-//         $(context).find('input.myCustom').once('mySecondBehavior').each(function () {
-//             if ($(this).visible()) {
-//                 $(this).css('background', 'green');
-//             }
-//             else {
-//                 $(this).css('background', 'yellow').show();
-//             }
-//         });
-//     }
-// };
